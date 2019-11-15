2d36049c0d13ab1205a5a4324507d64b60b167fb
Daniel <olgierd.dan@gmail.com>
2019-11-15 22:05:58 +0100

    Modify getInitialUnitFeatureCollection method

--- src/app/home/services/map.service.ts
+++ src/app/home/services/map.service.ts
@@ -2,18 +2,35 @@ import { Injectable } from "@angular/core";
 import { ApiJSONService } from "src/app/core/api/api-json.service";
 import { UnitPositionModel } from "../models/UnitPositionModels";
 import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
+import { map, tap } from "rxjs/operators";
+import { PositionService } from "./position.service";
 
 @Injectable({
   providedIn: "root"
 })
 export class MapServiceCustom {
-  constructor(private readonly apiJSONService: ApiJSONService) {}
+  // Keeping reference to last featureCollection
+  //  to avoid situation, where SignalR hub returns <5 results, and units
+  // temporarily disappear from the map
+  private _latestUnitFeatureCollection: GeoJSON.FeatureCollection<
+    GeoJSON.Point
+  >;
 
-  getUnitFeatures(): Observable<GeoJSON.Feature<GeoJSON.Point>[]> {
+  constructor(
+    private readonly apiJSONService: ApiJSONService,
+    private readonly positionService: PositionService
+  ) {}
+
+  getInitialUnitFeatureCollection(): Observable<
+    GeoJSON.FeatureCollection<GeoJSON.Point>
+  > {
     return this.apiJSONService.getUnitPositions().pipe(
-      map(units => {
-        return units.map(this.createUnitFeature);
+      map((units: UnitPositionModel[]) => {
+        this._latestUnitFeatureCollection = {
+          type: "FeatureCollection",
+          features: units.map(this.createUnitFeature)
+        };
+        return this._latestUnitFeatureCollection;
       })
     );
   }
