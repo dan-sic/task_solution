3bd4e291277823fac15bc5e94ce2e7f9e9d7dbbf
Daniel <olgierd.dan@gmail.com>
2019-11-15 16:55:38 +0100

    Implement filtering of units based on search input

--- src/app/units/services/unit.service.ts
+++ src/app/units/services/unit.service.ts
@@ -1,21 +1,26 @@
-import { Injectable } from "@angular/core";
+import { Injectable, OnDestroy } from "@angular/core";
 import { UnitModel } from "../components/unit-list/UnitModel";
-import { Subject, Observable } from "rxjs";
+import { Subject, Observable, Subscription } from "rxjs";
 import { ApiJSONService } from "src/app/core/api/api-json.service";
 import { take } from "rxjs/operators";
+import { SearchService } from "src/app/core/sidebar/service/search.service";
 
 @Injectable({
   providedIn: "root"
 })
-export class UnitService {
+export class UnitService implements OnDestroy {
   private _allUnits: UnitModel[];
   private _unitsToDisplay: Subject<UnitModel[]> = new Subject();
+  private _searchTextInputSubscription: Subscription;
 
   public unitsToDisplay$: Observable<
     UnitModel[]
   > = this._unitsToDisplay.asObservable();
 
-  constructor(private apiService: ApiJSONService) {
+  constructor(
+    private readonly apiService: ApiJSONService,
+    private readonly searchService: SearchService
+  ) {
     this.apiService
       .getUnits()
       .pipe(take(1))
@@ -23,5 +28,37 @@ export class UnitService {
         this._allUnits = units;
         this._unitsToDisplay.next(this._allUnits);
       });
+
+    this.listenToSearchTextInput();
+  }
+
+  ngOnDestroy() {
+    this._searchTextInputSubscription.unsubscribe();
+  }
+
+  private listenToSearchTextInput() {
+    this._searchTextInputSubscription = this.searchService.searchInputText$.subscribe(
+      input => {
+        this.filterOutUnits(input);
+      }
+    );
+  }
+
+  private filterOutUnits(searchInputValue: string) {
+    const filteredOutUnits = [];
+
+    const regex = new RegExp(searchInputValue, "i");
+
+    for (let unit of this._allUnits) {
+      for (let tag of unit.unitTag) {
+        const tagValueMatches = regex.test(tag.value);
+
+        if (tagValueMatches) {
+          filteredOutUnits.push(unit);
+          break;
+        }
+      }
+    }
+    this._unitsToDisplay.next(filteredOutUnits);
   }
 }
